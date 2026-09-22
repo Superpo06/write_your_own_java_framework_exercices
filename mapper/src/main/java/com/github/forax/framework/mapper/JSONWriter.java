883844c554450602f53be60f963f48f327c27bd8
Dumas Pierre-Olivier <po94.dumas@gmail.com>
2026-09-22 12:07:34 +0200

    Q4 with a bit of JSONproperty

--- mapper/src/main/java/com/github/forax/framework/mapper/JSONWriter.java
+++ mapper/src/main/java/com/github/forax/framework/mapper/JSONWriter.java
@@ -6,34 +6,47 @@ import java.util.StringJoiner;
 import java.util.stream.Collectors;
 
 public final class JSONWriter {
-    static class PropertyDescriptorsCache extends ClassValue<PropertyDescriptor[]> {
+    static class GeneratorCache extends ClassValue<Generator> {
         @Override
-        protected PropertyDescriptor[] computeValue(Class<?> type) {
+        protected Generator computeValue(Class<?> type) {
             var beanInfo = Utils.beanInfo(type);
-            return beanInfo.getPropertyDescriptors();
+            var generators = Arrays.stream(beanInfo.getPropertyDescriptors())
+                    .filter(property -> !property.getName().equals("class"))
+                    .filter(property -> property.getReadMethod() != null)
+                    .<Generator>map(property ->{
+                        var name = property.getName();
+                        var getter = property.getReadMethod();
+                        var jsonProperty = getter.getAnnotation(JSONProperty.class);
+                        if (jsonProperty != null) {
+                            name = jsonProperty.value();
+                        }
+                        var prefix = "\"" + name + "\": ";
+                        return (writer, bean) -> {
+                            var value = Utils.invokeMethod(bean, getter);
+                            return prefix + writer.toJSON(value);
+                        };
+                    })
+                    .toList();
+
+            return (writer, bean) -> generators.stream()
+                        .map(generator -> generator.generate(writer, bean))
+                        .collect(Collectors.joining(", ", "{", "}"));
         }
     }
 
+    private interface Generator {
+        String generate(JSONWriter writer, Object Bean);
+    }
 
-
-    private static final PropertyDescriptorsCache CACHE = new PropertyDescriptorsCache();
+    private static final GeneratorCache CACHE = new GeneratorCache();
     public String toJSON(Object o) {
     return switch(o){
         case Integer _, Boolean _, Double _ -> "" + o;
         case String s -> "\"" + s + "\"";
         case null -> "null";
         case Object obj -> {
-            var properties = CACHE.get(obj.getClass());
-            yield Arrays.stream(properties)
-                    .filter(property -> !property.getName().equals("class"))
-                    .filter(property -> property.getReadMethod() != null)
-                    .map(property -> {
-                        var name = property.getName();
-                        var getter = property.getReadMethod();
-                        var value = Utils.invokeMethod(obj, getter);
-                        return "\"" + name + "\": " + toJSON(value);
-                    })
-                    .collect(Collectors.joining(", ", "{", "}"));
+            var generator = CACHE.get(obj.getClass());
+            yield generator.generate(this, obj);
         }
     };
   }
